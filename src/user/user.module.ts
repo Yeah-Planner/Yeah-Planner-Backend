df5a1fe9b68a78be95c0a664c921463730e05a03
YEAHx4 <developerstarlight@gmail.com>
2021-11-25 01:10:06 +0900

    subscriber

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
+import { UserSubscriber } from './user.subscriber';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserSubscriber],
 })
 export class UserModule {}
