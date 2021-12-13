3b12db03e92c67bf9b4e3f401e747945de571e02
YEAHx4 <developerstarlight@gmail.com>
2021-12-13 13:53:28 +0900

    logger config

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'verbose', 'log'],
+  });
   app.enableCors({
     origin: (origin, callback) => {
       const o = origin.split('://')[1].split(':')[0];
